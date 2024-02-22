dd70e70ff8a1f6b24098254de8d0ca76dc87312e
Sai Tejas Janjur <youremail@example.com>
2024-02-21 23:46:06 -0600

    readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,17 @@ more_movies = [
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
     :release_date => '13-Aug-2021'}
+  {:title => 'Spirited Away', :rating => 'PG',
+   :release_date => '20-July-2001'}
+  {:title => 'Inception', :rating => 'PG-13',
+   :release_date => '16-July-2010'}
+  {:title => 'The Lion King', :rating => 'G',
+   :release_date => '24-June-1994'}
+  {:title => 'Interstellar', :rating => 'PG-13',
+   :release_date => '7-November-2014'}
+    
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  Movie.create!(movie) unless Movie.exists?(title: movie[:title]) 
 end
